837d7c21bc34e505ce8f132223cca420f7771238
taisei1223 <taisei12232000m@gmail.com>
2024-08-03 11:00:11 +0900

    test

--- 20240722/read_csv.py
+++ 20240722/read_csv.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import time
+import csv 
 
 filepath=''
 
-data = pd.read_csv(filepath)
+#data = pd.read_csv(filepath)
 
-data
-
-for i in range(0,100):
-    print(i)
-    time.sleep(2)
+#data
+    
+with open(filepath, encoding='utf8', newline='') as f:
+    csvreader = csv.reader(f)
+    content = [row for row in csvreader]   
+    
+for i in range(1,101):
+    print(content[i])
+    time.sleep(1)
+    print(content[i][1])
+    
+    
+    
